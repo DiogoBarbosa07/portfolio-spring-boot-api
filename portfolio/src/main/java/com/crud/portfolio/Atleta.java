853f957305bf8bc4888a863ffffcfb49bc5694d1
DiogoBarbosa07 <DiogoDev2023@outlook.com>
2024-05-07 15:50:30 -0300

    Criação da interface AtletaRepository

---
+++ portfolio/src/main/java/com/crud/portfolio/Atleta.java
@@ -0,0 +1,25 @@
+package com.crud.portfolio;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Atleta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int idade;
+    private String modalidade;
+
+    
+}
